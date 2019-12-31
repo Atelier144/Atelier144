6b1a8d68a1c3cf00cbf4d9446ecb92474e67800b
Atelier144 <amestric16@gmail.com>
2019-12-31 22:49:29 +0900

    SignupのjQueryを無効化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,87 +51,6 @@ $(document).ready(function () {
         interval: 10000
     });
 
-    $("#js-signup-form-username").on("change", function () {
-        var username = $(this).prop("value");
-        if (username === "") {
-            $(this).removeClass("ok").addClass("warning");
-            $("#js-signup-message-username").text("ユーザー名を入力してください").show().removeClass("ok").addClass("warning");
-        } else {
-            $(this).removeClass("warning").addClass("ok");
-            $("#js-signup-message-username").text("OK").show().removeClass("warning").addClass("ok");
-        }
-        signupConfirmation();
-    });
-
-    $("#js-signup-form-email").on("change", function () {
-        var email = $(this).prop("value");
-        if (email.match(emailRegExp)) {
-            $.ajax({
-                url: "signup/email_confirmation",
-                type: "post",
-                data: {
-                    email: email
-                },
-                dataType: "json",
-                success: function (data) {
-                    if (data === null) {
-                        $("#js-signup-form-email").removeClass("warning").addClass("ok");
-                        $("#js-signup-message-email").text("OK").show().removeClass("warning").addClass("ok");
-                    } else {
-                        $("#js-signup-form-email").removeClass("ok").addClass("warning");
-                        $("#js-signup-message-email").text("そのメールアドレスは既に使用されています").show().removeClass("ok").addClass("warning");
-                    }
-                },
-                error: function (data) {
-                    $(this).removeClass("ok").addClass("warning");
-                    $("#js-signup-message-email").text("サーバー内部でエラーが発生しました。").show().removeClass("ok").addClass("warning");
-                }
-            });
-        } else {
-            $(this).removeClass("ok").addClass("warning");
-            $("#js-signup-message-email").text("メールアドレスを入力してください").show().removeClass("ok").addClass("warning");
-        }
-        signupConfirmation();
-    });
-
-    $("#js-signup-form-password").on("change", function () {
-
-        var password = $("#js-signup-form-password").prop("value");
-        if (password === "") {
-            $("#js-signup-form-password").removeClass("ok").addClass("warning");
-            $("#js-signup-message-password").text("パスワードを入力してください").show().removeClass("ok").addClass("warning");
-            $("#js-signup-form-password-confirmation").prop("disabled", "disabled");
-
-        } else if (password.length < 8 || password.length > 32) {
-            $("#js-signup-form-password").removeClass("ok").addClass("warning");
-            $("#js-signup-message-password").text("8字以上、32字以下のパスワードを入力してください").show().removeClass("ok").addClass("warning");
-            $("#js-signup-form-password-confirmation").prop("disabled", "disabled");
-        } else {
-            $("#js-signup-form-password").removeClass("warning").addClass("ok");
-            $("#js-signup-message-password").text("OK").show().removeClass("warning").addClass("ok");
-            $("#js-signup-form-password-confirmation").prop("disabled", "");
-        }
-        $("#js-signup-form-password-confirmation").prop("value", "").removeClass("ok").removeClass("warning");;
-        $("#js-signup-message-password-confirmation").hide();
-    });
-
-
-    $("#js-signup-form-password-confirmation").on("change", function () {
-        var password = $("#js-signup-form-password").prop("value");
-        var passwordConfirmation = $("#js-signup-form-password-confirmation").prop("value");
-        if (passwordConfirmation === "") {
-            $("#js-signup-form-password-confirmation").removeClass("ok").addClass("warning");
-            $("#js-signup-message-password-confirmation").text("パスワードを再入力してください").show().removeClass("ok").addClass("warning");
-        } else if (password !== passwordConfirmation) {
-            $("#js-signup-form-password-confirmation").removeClass("ok").addClass("warning");
-            $("#js-signup-message-password-confirmation").text("もう一度入力してください").show().removeClass("ok").addClass("warning");
-        } else {
-            $("#js-signup-form-password-confirmation").removeClass("warning").addClass("ok");
-            $("#js-signup-message-password-confirmation").text("OK").show().removeClass("warning").addClass("ok");
-        }
-        signupConfirmation();
-    });
-
     function signupConfirmation() {
         var isUsernameOK = $("#js-signup-form-username").hasClass("ok");
         var isEmailOK = $("#js-signup-form-email").hasClass("ok");
@@ -144,7 +63,6 @@ $(document).ready(function () {
         }
     }
 
-    */
     $("#js-setting-image-select").click(function () {
         $("#js-setting-image-file").click();
     });
